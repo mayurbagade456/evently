5a50753f317c3c236aa686c4bd5db76e8c6e4cdf
mayurbagade456 <mayurbagade456@gmail.com>
2024-04-28 19:41:17 +0530

    first commit

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -83,12 +83,12 @@ export async function POST(req: Request) {
     const {id, image_url, first_name, last_name, username } = evt.data
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name!,
+      lastName: last_name!,
       username: username!,
-      photo: image_url,
+      photo: image_url!,
     }
-
+    
     const updatedUser = await updateUser(id, user)
 
     return NextResponse.json({ message: 'OK', user: updatedUser })
