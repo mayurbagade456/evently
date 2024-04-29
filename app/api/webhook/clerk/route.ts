b0758e6550985f31d09035b2dcc01db5504cc342
mayurbagade456 <109091739+mayurbagade456@users.noreply.github.com>
2024-04-29 20:50:24 +0530

    Update route.ts

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -54,21 +54,26 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
-  if (eventType === "user.created") {
-    const { email_addresses, image_url, first_name, last_name, username } =
-      evt.data;
+if (eventType === "user.created") {
+  const { id, email_addresses, image_url, first_name, last_name, username } =
+    evt.data;
 
-    const newUser = await createUser({
-      clerkId: id,
-      email: email_addresses[0].email_address,
-      username: username || "",
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
-    });
-
-    return NextResponse.json({ message: "OK", user: newUser });
+  if (!id) {
+    throw new Error("User ID is undefined");
   }
+
+  const newUser = await createUser({
+    clerkId: id,
+    email: email_addresses[0].email_address,
+    username: username || "",
+    firstName: first_name,
+    lastName: last_name,
+    photo: image_url,
+  });
+
+  return NextResponse.json({ message: "OK", user: newUser });
+}
+
 
   if (eventType === "user.updated") {
     const { image_url, first_name, last_name, username } = evt.data;
